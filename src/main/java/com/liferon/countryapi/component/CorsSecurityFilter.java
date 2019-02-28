86ff067783d37ddbaed65fbdbce51c91ed52f154
Ebenezer Olanrewaju <ebenezer.olanrewaju@venturegardengroup.com>
2019-02-28 23:21:36 +0100

    Added filter class to redirect token requests

---
+++ src/main/java/com/liferon/countryapi/component/CorsSecurityFilter.java
@@ -0,0 +1,63 @@
+package com.liferon.countryapi.component;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsSecurityFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+        response.addHeader("Access-Control-Expose-Headers", "x-requested-with");
+
+        MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest(request);
+        String ipAddress = request.getHeader("X-FORWARDED-FOR");
+        if (ipAddress == null || ipAddress.isEmpty()) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        if (request.getMethod().equalsIgnoreCase("POST") && request.getRequestURI().equals("/oauth/token") && request.getHeader("Authorization") == null) {
+
+            String rawHeaderString = "";//client.getClientId()+":"+client.getClientSecret();
+            String authHeader = Base64.encodeBase64String(rawHeaderString.getBytes());
+
+
+            mutableRequest.putHeader("Authorization", "Basic "+authHeader);
+
+        }
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            //response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            try {
+                filterChain.doFilter(mutableRequest, response);
+            } catch (IOException | ServletException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- src/main/java/com/liferon/countryapi/resource/UserResource.java
+++ src/main/java/com/liferon/countryapi/resource/UserResource.java
@@ -2,23 +2,31 @@ package com.liferon.countryapi.resource;
 
 import com.liferon.countryapi.model.LoginModel;
 import com.liferon.countryapi.model.UserModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.View;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
 public class UserResource {
 
+    @PostMapping("/register")
     public ResponseEntity<?> signUp(@Valid @RequestBody UserModel userModel, BindingResult result) {
 
 
         return ResponseEntity.ok("");
     }
 
-    public ResponseEntity<?> login(@Valid @RequestBody LoginModel loginModel, BindingResult result) {
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginModel loginModel, BindingResult result, HttpServletRequest request) {
+            request.setAttribute(
+                    View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT);
 
         return ResponseEntity.ok("");
     }
